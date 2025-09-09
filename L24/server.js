9011a3ad1f5883cae9c40ff6a16b4dc39604adbe
Pranays2512 <pranay.s2512@gmail.com>
2025-09-09 10:28:16 +0530

    commit this

--- L24/server.js
+++ L24/server.js
@@ -9,18 +9,18 @@ app.use(express.json());
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Needed to get __dirname in ES modules
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files (e.g., HTML, CSS, JS) from "public" folder
+
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
+
 app.use("/api/users", userRoutes);
 app.use("/api/blogs", blogRoutes);
 
-// MongoDB connection
+
 mongoose.connect("mongodb://localhost:27017/lec17")
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
